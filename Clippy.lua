f1e6fae1498861d3dc6a8a484d65b40b86e3e3ae
yoshiyuka <yoshiyuka@users.noreply.github.com>
2015-07-27 00:20:26 -0700

    Chat functions hooked and working now.

--- Clippy.lua
+++ Clippy.lua
@@ -2,7 +2,24 @@
 Clippy
 ]]
 
-message("FUCK")
-function test()
-   message("test")
+local Clippy = CreateFrame("FRAME", "ClippyFrame")
+
+Clippy:RegisterEvent("CHAT_MSG");
+
+local function eventHandler(self, event, ...)
+	print("AN EVENT");
 end
+
+Clippy:SetScript("OnEvent", eventHandler);
+
+function Clippy_ChatEdit_ParseText(text, send)
+	if(send == 1) then
+		text:SetText(text:GetText() .. " wooo");
+	end
+end
+function Clippy_SendChatMessage(text, chatType, languageIndex, channel)
+	message("clippy send chat message");
+end
+
+hooksecurefunc("ChatEdit_ParseText", Clippy_ChatEdit_ParseText)
+hooksecurefunc("SendChatMessage", Clippy_SendChatMessage)
