5b8f447c3d94b1c9cd558d1d7ea2bc7e4bd899f3
yoshiyuka <yoshiyuka@users.noreply.github.com>
2015-10-17 10:47:47 -0700

    Basic new and add commands to create substitutes for a specified keyword.

--- Clippy.lua
+++ Clippy.lua
@@ -5,6 +5,7 @@ local substitutes = {}
 function Clippy:OnInitialize()
   	Clippy:RegisterChatCommand("clippy", "Clippy")
   	Clippy:RegisterChatCommand("clippy-new", "NewSubstitute")
+  	Clippy:RegisterChatCommand("clippy-add", "AddSubstitute")
   	Clippy:RegisterChatCommand("clippy-remove", "RemoveSubstitute")
   	Clippy:RegisterChatCommand("clippy-clear", "ClearSubstitutes")
   	Clippy:RegisterChatCommand("clippy-replace", "ReplaceSubstitute")
@@ -41,7 +42,23 @@ end
 
 function Clippy:NewSubstitute(arguments)
 	local name, default = Clippy:GetArgs(arguments, 2)
-	substitutes[name] = default
+	if(substitutes[name] == nil) then
+		print("Setting " .. name .. " in substitutes table with default substitution of: " .. default)
+		substitutes[name] = {default}
+	else
+		print("Substitute(s) already exist for " .. name)
+	end
+end
+
+function Clippy:AddSubstitute(arguments)
+	local name, sub = Clippy:GetArgs(arguments, 2)
+	if(substitutes[name] ~= nil) then
+		print("Adding " .. sub .. " to potential substitutes for " .. name)
+		table.insert(substitutes[name], sub)
+	else
+		print(name .. " is not an existing substitute table. Creating a new table for " .. name)
+		Clippy:NewSubstitute(arguments)
+	end
 end
 
 function Clippy:RemoveSubstitute(arguments)
