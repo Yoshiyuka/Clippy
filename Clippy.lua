4d2f743c66be2c5435c40c98d3982b3f90fd5031
yoshiyuka <yoshiyuka@users.noreply.github.com>
2015-10-17 15:17:33 -0700

    AceDB added to save substitutes between sessions.

--- Clippy.lua
+++ Clippy.lua
@@ -3,6 +3,14 @@ local Clippy = LibStub("AceAddon-3.0"):NewAddon("Clippy", "AceConsole-3.0", "Ace
 local substitutes = {}
 
 function Clippy:OnInitialize()
+	print("on init")
+	self.db = LibStub("AceDB-3.0"):New("ClippyDB")
+	if(self.db.profile.substitutes ~= nil) then
+		print("db substitutes not nil. Loading.")
+		substitutes = self.db.profile.substitutes
+	else
+		print("db substitutes is nil. Ignoring.")
+	end
   	Clippy:RegisterChatCommand("clippy", "Clippy")
   	Clippy:RegisterChatCommand("clippy-new", "NewSubstitute")
   	Clippy:RegisterChatCommand("clippy-add", "AddSubstitute")
@@ -13,11 +21,19 @@ function Clippy:OnInitialize()
 end
 
 function Clippy:OnEnable()
+	print("On Enable")
     Clippy:SecureHook("ChatEdit_ParseText")
 end
 
+function Clippy:OnLoad()
+	print("On Load")
+	if(self.db.profile.substitutes ~= nil) then
+		substitutes = self.db.profile.substitutes
+	end
+end
+
 function Clippy:OnDisable()
-	AceGUI:Release(ClippyFrame)
+	self.db.profile.substitutes = substitutes
 end
 
 function Clippy:ChatEdit_ParseText(chat, send)
@@ -29,7 +45,7 @@ function Clippy:ChatEdit_ParseText(chat, send)
 end
 
 function Clippy:ParseText(text)
-	if(#substitutes[text] > 0) then
+	if(substitutes[text] ~= nil) then
 		return (substitutes[text][math.random(#substitutes[text])])
 	else
 		return text
@@ -44,6 +60,7 @@ function Clippy:NewSubstitute(arguments)
 	if(substitutes[name] == nil) then
 		print("Setting " .. name .. " in substitutes table with default substitution of: " .. default)
 		substitutes[name] = {default}
+		self.db.profile.substitutes = substitutes
 	else
 		print("Substitute(s) already exist for " .. name)
 	end
@@ -54,6 +71,7 @@ function Clippy:AddSubstitute(arguments)
 	if(substitutes[name] ~= nil) then
 		print("Adding " .. sub .. " to potential substitutes for " .. name)
 		table.insert(substitutes[name], sub)
+		table.insert(self.db.profile.substitutes[name], sub)
 	else
 		print(name .. " is not an existing substitute table. Creating a new table for " .. name)
 		Clippy:NewSubstitute(arguments)
@@ -69,6 +87,11 @@ function Clippy:ClearSubstitutes(arguments)
 	for k in pairs(substitutes[name]) do
 		substitutes[name][k] = nil
 	end
+	for k in pairs(self.db.profile.substitutes[name]) do
+		self.db.profile.substitutes[name][k] = nil
+	end
+	substitutes[name] = nil
+	self.db.profile.substitutes[name] = nil
 end
 
 function Clippy:ReplaceSubstitute(arguments)
